903566e9f43e132f7a0d471a91c72b5d1fe624fc
jhoon9494 <wjdgns9406@gmail.com>
2021-10-19 17:40:19 +0900

    divide income, expend & add Array

--- js/hiddenForm.js
+++ js/hiddenForm.js
@@ -1,7 +1,6 @@
 // +버튼 클릭 시 수입 및 지출을 입력할 수 있는 폼이 나타나도록 함.
 
 const inputCashForm = document.querySelector("#inputCashForm");
-const addCash = document.querySelector("#addCash");
 const hideBtn = document.querySelector("#hideBtn");
 const hiddenFormInBtn = document.querySelector("#innerBtn");
 
--- js/inputCash.js
+++ js/inputCash.js
@@ -1,21 +1,57 @@
-//금액을 입력하고 저장하기 버튼을 눌렀을 때 전체 내역 자리로 데이터를 집어넣기(카테고리 value와 함께)
+//금액을 입력하고 저장하기 버튼을 눌렀을 때 합계금액, 수입 및 지출합계 자리로 데이터를 집어넣기(카테고리 text와 함께)
 const savebtn = inputCashForm.querySelector("#saveBtn");
+let incomeArr = []; //수입 빈 배열
+let expendArr = []; //지출 빈 배열
 
 function saveCash(event){
   event.preventDefault();
-  const inputCash = inputCashForm.querySelector(".inputCash");
-  const inputComment = inputCashForm.querySelector(".inputComment");
+  const inputCash = inputCashForm.querySelector(".inputCash"); //금액
+  const inputComment = inputCashForm.querySelector(".inputComment"); //사용 내역
+  const category = inputCashForm.querySelector(".category"); // 카테고리 선택
+  const incomeBtn = inputCashForm.querySelectorAll(".radioBtn")[0]; //수입 지출 구분
+  const expendBtn = inputCashForm.querySelectorAll(".radioBtn")[1]; //수입 지출 구분
+  
   const li = document.createElement("li");
   const span = document.createElement("span");
-  if(inputCash.value !== ""){ 
-    span.innerText = `${inputComment.value} ${inputCash.value}`;
+  const button = document.createElement("button");
+  button.innerText = "🗑";
+  button.addEventListener("click", deleteCash);
 
-    li.appendChild(span);
-    document.querySelector("#detail").appendChild(li);
+  if(inputCash.value !== ""){
+    if(incomeBtn.checked === true){ //수입 체크되었을 때 실행
+      span.innerText = `${category.value} ${inputCash.value} ${inputComment.value}`;
+      incomeArr.push(inputCash.value);
 
-    inputCash.value = "";
-    inputComment.value = "";
+      li.appendChild(span);
+      li.appendChild(button);
+      document.querySelector("#detail").appendChild(li);
+    } else if(expendBtn.checked === true){ //지출 체크되었을 때 실행
+      span.innerText = `${category.value} -${inputCash.value} ${inputComment.value}`;
+      expendArr.push(inputCash.value);
+
+      li.appendChild(span);
+      li.appendChild(button);
+      document.querySelector("#detail").appendChild(li);
+    }
+    
+    inputCash.value = ""; //입력 후 금액 입력란 초기화
+    inputComment.value = "";//입력 후 사용 내역 입력란 초기화
+  } else {
+    console.log("금액을 입력해 주세요.");
   }
+}
+
+function accountCash(arr){ //누적 금액 계산
+  let sum = 0;
+  for(let i = 0; i < arr.length; i++){
+    sum += arr[i];
+  }
+  return sum;
+}
+
+function deleteCash(event){ //내역 삭제
+  const li = event.target.parentElement;
+  li.remove();
 }
 
 savebtn.addEventListener("click", saveCash);

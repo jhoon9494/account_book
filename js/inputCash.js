91482996ef423ffa25c6f8fd547ae451e460be66
jhoon9494 <wjdgns9406@gmail.com>
2021-10-20 04:54:59 +0900

    indicate cash

--- js/inputCash.js
+++ js/inputCash.js
@@ -3,21 +3,22 @@ const savebtn = inputCashForm.querySelector("#saveBtn");
 let incomeArr = []; //수입 빈 배열
 let expendArr = []; //지출 빈 배열
 
-function saveCash(event){
+function saveCash(event){ //수입 및 지출금액 입력 후 저장버튼 클릭시 발생되는 함수
   event.preventDefault();
-  const inputCash = inputCashForm.querySelector(".inputCash"); //금액
-  const inputComment = inputCashForm.querySelector(".inputComment"); //사용 내역
+  const inputCash = inputCashForm.querySelector(".inputCash"); //금액 입력란
+  const inputComment = inputCashForm.querySelector(".inputComment"); //사용 내역 입력란
   const category = inputCashForm.querySelector(".category"); // 카테고리 선택
-  const incomeBtn = inputCashForm.querySelectorAll(".radioBtn")[0]; //수입 지출 구분
-  const expendBtn = inputCashForm.querySelectorAll(".radioBtn")[1]; //수입 지출 구분
+  const incomeBtn = inputCashForm.querySelectorAll(".radioBtn")[0]; //수입 버튼(radio)
+  const expendBtn = inputCashForm.querySelectorAll(".radioBtn")[1]; //지출 버튼(radio)
   
+  //세부 내역을 리스트(ul)에 넣기 위한 요소 생성
   const li = document.createElement("li");
   const span = document.createElement("span");
   const button = document.createElement("button");
   button.innerText = "🗑";
   button.addEventListener("click", deleteCash);
 
-  if(inputCash.value !== ""){
+  if(inputCash.value !== ""){ //금액에 값 입력했을 경우에만 실행
     if(incomeBtn.checked === true){ //수입 체크되었을 때 실행
       span.innerText = `${category.value} ${inputCash.value} ${inputComment.value}`;
       incomeArr.push(inputCash.value);
@@ -37,20 +38,27 @@ function saveCash(event){
     inputCash.value = ""; //입력 후 금액 입력란 초기화
     inputComment.value = "";//입력 후 사용 내역 입력란 초기화
   } else {
-    console.log("금액을 입력해 주세요.");
+    alert("금액을 입력해 주세요.");
   }
+  const incomeCash = accountCash(incomeArr); //수입 합계 
+  document.querySelector("#totalIncome").querySelector("span").innerText = incomeCash;
+  const expendCash = accountCash(expendArr); //지출 합계
+  document.querySelector("#totalExpend").querySelector("span").innerText = -expendCash;
+  const incomePlusExpend = incomeCash - expendCash; //합계 금액
+  document.querySelector("#totalCash").querySelector("span").innerText = incomePlusExpend;
 }
 
 function accountCash(arr){ //누적 금액 계산
   let sum = 0;
   for(let i = 0; i < arr.length; i++){
-    sum += arr[i];
+    sum += parseInt(arr[i]); //Number type으로 변환하기 위해 parseInt 사용.
   }
   return sum;
 }
 
 function deleteCash(event){ //내역 삭제
   const li = event.target.parentElement;
+  const textLi = li.firstChild.firstChild.data; //삭제 버튼 클릭 시, 해당 버튼의 부모인 <li> 내부에 금액 값
   li.remove();
 }
 
